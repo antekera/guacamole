553cf97f7ea6bf44e5cd1c9e3818ed0902085364
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-04-15 19:46:35 -0400

    add test mapper

---
+++ src/lib/keysToComponentMap.ts
@@ -0,0 +1,14 @@
+import { Container } from '../components/container'
+
+export const keysToComponentMap = {
+  card: Container,
+  body: Container,
+  body2: Container,
+  body3: Container,
+  body4: Container,
+  body5: Container,
+  title: Container,
+  subtitle: Container,
+  text: Container,
+  button: Container,
+}
