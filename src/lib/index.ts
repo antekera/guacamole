920b8204095a900183e03f0ea15984e1e83ca515
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-04-15 19:47:09 -0400

    add index lib

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Home data
+ */
+
+import { fetcher } from './fetcher'
+import { keysToComponentMap } from './keysToComponentMap'
+import { Renderer } from './renderer'
+
+export { fetcher, keysToComponentMap, Renderer }
