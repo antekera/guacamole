71531730b524afee480f0b7ddfdf2859e38e6a0c
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-04-16 19:53:10 -0400

    map components to first level props

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,13 +1,29 @@
-import deepmerge from 'deepmerge'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as R from 'ramda'
 
 import { components, content, styles } from '../../data/home'
+
+const props = R.mergeDeepLeft(content, styles)
+
+const matchComponentsProps = comps => {
+  Object.keys(comps).map(id => {
+    const { children } = comps[id]
+    if (!R.isNil(children)) {
+      matchComponentsProps(children)
+    }
+    if (Object.keys(props).includes(id)) {
+      comps[id] = { ...comps[id], ...props[id] }
+    }
+  })
+
+  return comps
+}
+
+const data = matchComponentsProps(components)
 
 type Data = {
   name: string
 }
-
-const data = deepmerge.all([components, content, styles])
 
 export default function handler(
   req: NextApiRequest,
